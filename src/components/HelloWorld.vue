c6c104a3ae86899e453fe7671547c4d369f379c4
Alexey Romanov <lekcrg@ya.ru>
2023-02-10 19:01:33 +0400

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="hello-world">
+    <h1 class="title">{{ msg }}</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .title {
+    font-size: 64px;
+    font-weight: normal;
+  }
+</style>
